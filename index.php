4cdb8bd9cb165423346f11b179c83d250c4e6b5e
ramdanu16 <146074271+ramdanu16@users.noreply.github.com>
2023-09-26 09:37:17 +0000

    update 26

--- index.php
+++ index.php
@@ -1,2 +1,29 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Login</title>
+    <link rel="stylesheet" href="style.css">         
+    <link rel="stylesheet" type="text/css" href="css/style.css" />             
+</head>
+<body>
+    <div class="login">
+        <form>
+            <h1>Login</h1>
+            <hr>
+            <p>Silahkan Login Terlebih Dahulu</p>
+            <Label>Email</Label>
+            <input type="text" placeholder="Masukan Email">
+            <label>Password</label>
+            <input type="password" placeholder="Masukan Password">
+            <button>Login</button>
+            <p>
+                <a href="#">Lupa password</a>
+            </p>
+            <clossform></clossform>
+        </form>
+    </div>
+</body>
+</html>
